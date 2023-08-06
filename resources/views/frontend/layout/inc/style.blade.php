12eb7fc7a134c4c5cbe4a0bc552f007ef53c6494
biplab990 <biplabuddin990@gmail.com>
2023-08-06 22:56:25 +0600

    up

--- resources/views/frontend/layout/inc/style.blade.php
+++ resources/views/frontend/layout/inc/style.blade.php
@@ -21,3 +21,4 @@
 <link rel="stylesheet" href="{{ asset('assets/frontend') }}/css/responsive.css">
 <!-- modernizr css -->
 <script src="{{ asset('assets/frontend') }}/js/vendor/modernizr-2.8.3.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
